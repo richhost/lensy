de27a419222e96132e52b0942ff6c954825da4dd
richhost <hexcy@pm.me>
2026-03-28 14:37:55 +0800

    feat: initialize project

---
+++ src/hooks.server.ts
@@ -0,0 +1,26 @@
+import type { Handle } from '@sveltejs/kit';
+import { building } from '$app/environment';
+import { createAuth } from '$lib/server/auth';
+import { svelteKitHandler } from 'better-auth/svelte-kit';
+
+const handleBetterAuth: Handle = async ({ event, resolve }) => {
+	if (!event.platform?.env?.DB)
+		throw new Error('D1 binding "DB" not found — are you running with wrangler?');
+
+	if (!event.platform?.env?.BUCKET)
+		throw new Error('R2 binding "BUCKET" not found — are you running with wrangler?');
+
+	event.locals.auth = createAuth(event.platform.env.DB);
+
+	const { auth } = event.locals;
+	const session = await auth.api.getSession({ headers: event.request.headers });
+
+	if (session) {
+		event.locals.session = session.session;
+		event.locals.user = session.user;
+	}
+
+	return svelteKitHandler({ event, resolve, auth, building });
+};
+
+export const handle: Handle = handleBetterAuth;
