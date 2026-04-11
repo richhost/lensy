1c3ffab2c4c73b395455a6d84b416719055d238d
richhost <hexcy@pm.me>
2026-04-11 11:25:38 +0800

    chore: update dependencies

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'drizzle-kit';
 
+if (!process.env.CLOUDFLARE_ACCOUNT_ID) throw new Error('CLOUDFLARE_ACCOUNT_ID is not set');
+if (!process.env.CLOUDFLARE_DATABASE_ID) throw new Error('CLOUDFLARE_DATABASE_ID is not set');
+if (!process.env.CLOUDFLARE_D1_TOKEN) throw new Error('CLOUDFLARE_D1_TOKEN is not set');
+
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'sqlite',
